d6f298a34ca70ff49f011504210998c87ba226f3
vishwesh5 <vishweshshrimali5@gmail.com>
2018-11-14 18:09:49 +0530

    Add results

--- getDataFromCSV.py
+++ getDataFromCSV.py
@@ -19,7 +19,7 @@ with open(csv_fname,'r') as f:
         totalLines = f.readlines()[1:]
     else:
         totalLines = f.readlines()[1:numLines]
-    print(len(totalLines))
+    #print(len(totalLines))
     for line in totalLines:
         try:
             qid1, qid2, q1, q2 = line.strip().split(',')[1:5]
@@ -39,8 +39,8 @@ print("Model loaded successfully")
 def plot_similarity(labels1, labels2, features1, features2, rotation):
     corr = np.inner(features1, features2)
     corr2 = corr.copy()
-    corr2[corr2<0.4]=0
-    corr2[corr2>=0.4]=1
+    corr2[corr2<0.8]=0
+    corr2[corr2>=0.8]=1
     #print(corr)
     sns.set(font_scale=0.6)
     #plt.figure(figsize=(100,100))
@@ -68,25 +68,35 @@ def plot_similarity(labels1, labels2, features1, features2, rotation):
     plt.tight_layout()
     plt.savefig("Quora-2.png")
     plt.show()
+    similar_qid = {}
+    for i in range(len(labels1)):
+        for j in range(len(labels2)):
+            if corr2[i][j] == 1:
+                similar_qid[labels1[i]]=labels2[j]
+    return similar_qid
 
 def run_and_plot(session_, input_tensor_, messages1_, messages2_, labels1_,labels2_, encoding_tensor):
     print("Embeddings questions 1")
     message_embeddings1_ = session_.run(encoding_tensor, feed_dict={input_tensor_: messages1_})
     print("Embeddings questions 2")
     message_embeddings2_ = session_.run(encoding_tensor, feed_dict={input_tensor_: messages2_})
-    plot_similarity(labels1_,labels2_, \
+    similar_qid = plot_similarity(labels1_,labels2_, \
             message_embeddings1_,\
             message_embeddings2_, 90)
+    return similar_qid
 
 similarity_input_placeholder = tf.placeholder(tf.string, shape=(None))
 similarity_message_encodings = embed(similarity_input_placeholder)
 with tf.Session() as session:
     session.run(tf.global_variables_initializer())
     session.run(tf.tables_initializer())
-    run_and_plot(session, similarity_input_placeholder,\
+    similar_qid = run_and_plot(session, similarity_input_placeholder,\
             list(question1.values()),\
             list(question2.values()),\
             list(question1.keys()),\
             list(question2.keys()),\
             similarity_message_encodings)
 
+with open("similarity-results.txt",'w') as f:
+    for i in list(similar_qid.keys()):
+        f.write("{},{}\n=======================\n".format(question1[i], question2[similar_qid[i]]))
